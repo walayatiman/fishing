3dd3a6b8d119a39519c279a84f77282f1497e91f
Jevan <48089409+walayatiman@users.noreply.github.com>
2024-01-12 22:30:03 +0800

    comment js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,22 +1,22 @@
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
+// import gsap from "gsap";
+// import ScrollTrigger from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".first", {
-  delay: 0.2,
-  left: "-100%",
-  ease: "expo.inOut",
-});
+// gsap.to(".first", {
+//   delay: 0.2,
+//   left: "-100%",
+//   ease: "expo.inOut",
+// });
 
-gsap.to(".second", {
-  delay: 0.4,
-  left: "-100%",
-  ease: "expo.inOut",
-});
+// gsap.to(".second", {
+//   delay: 0.4,
+//   left: "-100%",
+//   ease: "expo.inOut",
+// });
 
-gsap.to(".third", {
-  delay: 0.6,
-  left: "-100%",
-  ease: "expo.inOut",
-});
+// gsap.to(".third", {
+//   delay: 0.6,
+//   left: "-100%",
+//   ease: "expo.inOut",
+// });
