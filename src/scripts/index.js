a8e574794406bac3a3d335081af5193654ba647a
Jevan <48089409+walayatiman@users.noreply.github.com>
2024-01-19 15:14:57 +0800

    added animation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+const menuHeader = document.querySelectorAll("#header menu li");
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -20,3 +21,14 @@ gsap.to(".third", {
   left: "-100%",
   ease: "expo.inOut",
 });
+gsap.fromTo(
+  "#header .container picture",
+  { opacity: 0, x: -20 },
+  { opacity: 1, x: 20, delay: 1 }
+);
+
+gsap.fromTo(
+  menuHeader,
+  { opacity: 0, x: -20 },
+  { opacity: 1, x: 0, stagger: 0.3, delay: 1.4 }
+);
